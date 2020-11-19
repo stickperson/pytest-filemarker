5731595ce603bfb11a11d81fcee936fc0561cdf6
Joe Meissler <joe.meissler@gmail.com>
2020-11-18 21:55:55 -0800

    tpyo

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name="pytest-filenarmer",
+    name="pytest-filemarker",
     packages=["pytest_filemarker"],
     # the following makes a plugin available to pytest
     entry_points={"pytest11": ["filemarker = pytest_filemarker.plugin"]},
